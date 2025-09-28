f982a1272b1c654e4e097f13a940f7be6311e9c5
malachisheehan <162676926+malachisheehan@users.noreply.github.com>
2025-09-28 15:59:35 -0500

    fixed bug

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
     int scoreOfPlayer1, scoreOfPlayer2 = 0;
     public GameUIController gameUI;
     int winScore = 4;
